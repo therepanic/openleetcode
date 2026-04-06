0d6e0e1aa0d92306506ce35df18d7bb289fa0a29
Andrey Litvitski <andrey1010102008@gmail.com>
2026-04-06 23:55:50 +0300

    implement github files downloader module

---
+++ cli/CLI/GitHub.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module CLI.GitHub where
+
+import CLI.AppEnv
+import Codec.Archive.Zip
+import Data.List (isPrefixOf)
+import Network.HTTP.Req
+
+-- todo: for runtimes, we could consider downloading files directly since there are few of them, and also perhaps optimize the current logic somehow.
+
+downloadTests :: IO ()
+downloadTests = do
+  archive <- downloadRepoArchive
+  let strip e = e {eRelativePath = drop (length ("openleetcode-main/" :: String)) (eRelativePath e)}
+      filtered =
+        archive
+          { zEntries =
+              map strip $
+                filter (isPrefixOf "openleetcode-main/tests/" . eRelativePath) $
+                  zEntries archive
+          }
+  root <- defaultConfigRoot
+  extractFilesFromArchive [OptDestination root] filtered
+
+downloadRuntimes :: IO ()
+downloadRuntimes = do
+  archive <- downloadRepoArchive
+  let strip e = e {eRelativePath = drop (length ("openleetcode-main/" :: String)) (eRelativePath e)}
+      filtered =
+        archive
+          { zEntries =
+              map strip $
+                filter (isPrefixOf "openleetcode-main/runtimes/" . eRelativePath) $
+                  zEntries archive
+          }
+  root <- defaultConfigRoot
+  extractFilesFromArchive [OptDestination root] filtered
+
+downloadRepoArchive :: IO Archive
+downloadRepoArchive = do
+  body <- runReq defaultHttpConfig $ do
+    -- note that we are taking the code from the main branch. also repo hardcoded
+    response <- req GET (https "github.com" /: "therepanic" /: "openleetcode" /: "archive" /: "refs" /: "heads" /: "main.zip") NoReqBody lbsResponse mempty
+    return (responseBody response)
+  let archive = toArchive body
+  return archive
