66b631094e8ca8ab7218a4e79fb34c647090ff15
Andrey Litvitski <andrey1010102008@gmail.com>
2026-04-07 21:38:57 +0300

    set approximate command architecture

---
+++ cli/CLI/Commands.hs
@@ -0,0 +1,7 @@
+module CLI.Commands where
+
+import Options.Applicative
+
+data Command
+
+commandParser :: Parser Command
--- cli/Main.hs
+++ cli/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import CLI.Commands
+import Options.Applicative
+
 main :: IO ()
 main = do
-  print "starting point"
+  cmd <-
+    execParser $
+      info
+        (commandParser <**> helper)
+        (fullDesc <> progDesc "we have democratized the Leetcode tests" <> header "openleetcode")
+  dispatch cmd
+
+dispatch :: Command -> IO ()
