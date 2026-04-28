a0676a70f24897ad229bcc267ec3a4153c8b9e2e
Andrey Litvitski <andrey1010102008@gmail.com>
2026-04-28 23:58:45 +0300

    add constructor to java utilities runtime

--- runtimes/java/utilities.java
+++ runtimes/java/utilities.java
@@ -1,4 +1,16 @@
 class TreeNode {
+	public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
     public static TreeNode toTreeNode(Integer[] arr) {
         if (arr == null || arr.length == 0 || arr[0] == null) return null;
         TreeNode root = new TreeNode(arr[0]);
