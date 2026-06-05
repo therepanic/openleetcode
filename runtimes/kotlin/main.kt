5c1d07a30b23acd6dd75c98dc46b522015a977b4
Andrey Litvitski <andrey1010102008@gmail.com>
2026-06-05 15:40:57 +0300

    repair kotlin runtime

--- runtimes/kotlin/main.kt
+++ runtimes/kotlin/main.kt
@@ -3,12 +3,13 @@ import java.time.temporal.TemporalAccessor
 import java.util.*
 import java.io.File
 import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
 
 import kotlin.math.*
 import java.lang.StringBuilder
 
-data class TestInput(var val: Any? = null)
+data class TestInput(@SerializedName("val") var value: Any? = null)
 
 val _TEST_: Map<String, Map<String, TestInput>> =
     Gson().fromJson(
@@ -16,7 +17,7 @@ val _TEST_: Map<String, Map<String, TestInput>> =
         object : TypeToken<Map<String, Map<String, TestInput>>>() {}.type
     )
 
-fun testVal(caseId: String, name: String): Any? = _TEST_[caseId]!![name]!!.val
+fun testVal(caseId: String, name: String): Any? = _TEST_[caseId]!![name]!!.value
 
 ${SOLUTION}
 
