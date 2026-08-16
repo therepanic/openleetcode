2ba97845ea45b4e7011244b1264da7706a173909
Andrey Litvitski <andrey1010102008@gmail.com>
2026-08-16 23:49:50 +0300

    lil more

--- tests/1501-2000/1848. minimum-distance-to-the-target-element/sol.py
+++ tests/1501-2000/1848. minimum-distance-to-the-target-element/sol.py
@@ -1,3 +1,5 @@
 class Solution:
     def getMinDistance(self, nums: list[int], target: int, start: int) -> int:
-        return min(abs(index - start) for index, value in enumerate(nums) if value == target)
+        return min(
+            abs(index - start) for index, value in enumerate(nums) if value == target
+        )
